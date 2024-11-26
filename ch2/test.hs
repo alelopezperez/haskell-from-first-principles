91ac29a30975023376f2d477b15ab28cd7a54eae
alelopezperez <alelopezperez.dev@gmail.com>
2024-11-26 12:21:59 -0400

    ch2

---
+++ ch2/test.hs
@@ -0,0 +1,14 @@
+-- test.hs
+
+-- Type Signature for variable sayHello : String->IO(), is String to IO, 
+-- that lambda calculus we only have one arguments, more than one argument is with curried funcionts
+sayHello :: String -> IO() 
+sayHello x = putStrLn("Hello, " ++ x ++ "!")
+
+-- Simple Inferred Function to triple an argument
+triple x = x * 3
+-- Triple x = x * 4 error trying to import  a module or a type.
+
+-- Area of a cicle
+area x = 3.14 * x * x
+
