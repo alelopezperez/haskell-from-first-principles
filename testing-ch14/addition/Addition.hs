2747b88b4b9d7d78899fb27a8273e48ff4027f92
alelopezperez <alelopezperez.dev@gmail.com>
2025-03-19 13:08:30 -0400

    morse started

--- testing-ch14/addition/Addition.hs
+++ testing-ch14/addition/Addition.hs
@@ -3,6 +3,56 @@ module Addition where
 import Test.Hspec
 import Test.QuickCheck
 
+genBool :: Gen Bool
+genBool = choose (False, True)
+genBool' :: Gen Bool
+genBool' = elements [False, True]
+genOrdering :: Gen Ordering
+genOrdering = elements [LT, EQ, GT]
+genChar :: Gen Char
+genChar = elements ['a' .. 'z']
+
+genTuple :: (Arbitrary a, Arbitrary b) => Gen (a, b)
+genTuple = do
+    a <- arbitrary
+    b <- arbitrary
+    return (a, b)
+
+genThreeple :: (Arbitrary a, Arbitrary b, Arbitrary c) => Gen (a, b, c)
+genThreeple = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    return (a, b, c)
+
+genEither :: (Arbitrary a, Arbitrary b) => Gen (Either a b)
+genEither = do
+    a <- arbitrary
+    b <- arbitrary
+    elements [Left a, Right b]
+
+-- equal probability
+genMaybe :: (Arbitrary a) => Gen (Maybe a)
+genMaybe = do
+    a <- arbitrary
+    elements [Nothing, Just a]
+
+-- What QuickCheck does so
+-- you get more Just values
+genMaybe' :: (Arbitrary a) => Gen (Maybe a)
+genMaybe' = do
+    a <- arbitrary
+    frequency
+        [ (1, return Nothing)
+        , (3, return (Just a))
+        ]
+
+prop_additionGreater :: Int -> Bool
+prop_additionGreater x = x + 1 > 0
+
+runQc :: IO ()
+runQc = quickCheck prop_additionGreater
+
 main :: IO ()
 main = hspec $ do
     describe "Addition" $ do
